a1542cd04d2a725016cc41a05dd1fcb8eb78c668
George Chearswat <chearswat@gmail.com>
2015-03-31 19:04:51 -0700

    Made progress up to about_iterations.rb Made notes on what to review later.

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,26 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == 0 or b == 0 or c == 0
+      raise TriangleError, "No sides of a triangle should be 0"
+  elsif a < 0 or b < 0 or c < 0
+      raise TriangleError, "No side of a triangle should be negative"
+  end
+
+  triangle_array = [a, b, c]
+  sorted_triangle_array = triangle_array.sort
+  if !((sorted_triangle_array[0] + sorted_triangle_array[1]) > sorted_triangle_array[2])
+       raise TriangleError, "Not a valid triangle size"
+  end
+
+
+  if (a == b and b == c and c == a)
+       return :equilateral
+  elsif (a == b || a == c || c == b)
+       return :isosceles
+  elsif a != b and a != c and c != b
+       return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.

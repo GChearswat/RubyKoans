5c2821869cbed7231ebf1b922a04d37724ed3aa4
George Chearswat <chearswat@gmail.com>
2015-04-02 17:25:39 -0700

    finished Ruby Koans. Quite interesting stuff. This is my first real step into ruby, and I'm going to delve into Rails next.

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+
+     attr_reader :values
+
+     def roll(dice_count)
+          i = 0
+          @values = []
+          while i < dice_count 
+               @values << (rand(6) + 1) 
+               i += 1
+          end
+     end          
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
